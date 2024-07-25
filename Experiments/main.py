51cc33063425c26e7cadea03eb961a121eaf62eb
sokong <annsokong@gmail.com>
2024-07-25 12:02:02 +0700

    Initial commit

---
+++ Experiments/main.py
@@ -0,0 +1,72 @@
+# from functions import get_todos, write_todos
+import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is ", now)
+text = """ 
+Principles of productivity:
+
+"""
+
+while True:
+    user_action = input("Type add ,show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith("add"):
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append(todo + '\n')
+
+        functions.write_todos(todos, 'files/todos.txt')
+
+    elif user_action.startswith("show"):
+
+        todos = functions.get_todos()
+        # print(todos)
+        # new_todos = [item.strip('\n') for item in todos]
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
+    elif user_action.startswith("edit"):
+        try:
+
+            number = int(user_action[5:])
+            number = number - 1
+
+            todos = functions.get_todos()
+            print('here is the existing todos', todos)
+
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+            print('Here is new how it will be', todos)
+
+            functions.write_todos(todos, 'files/todos.txt')
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+            index = number - 1
+            todos_to_removed = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"Todo {todos_to_removed} was removed from the list"
+            print(message)
+        except IndexError:
+            print("Command is not valid")
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Command is not exist")
+
+print("Bye")
